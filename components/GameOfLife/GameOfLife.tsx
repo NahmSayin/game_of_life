c2f7c58bc8a43a740ef0d5b613728ab57cbb2af9
jackvdp <jack@pumpymusic.co.uk>
2022-05-30 12:46:00 +0100

    create grid with click funcrion

--- components/GameOfLife/GameOfLife.tsx
+++ components/GameOfLife/GameOfLife.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Styles from './GameOfLife.module.css';
+import { Grid } from '../Grid'
 
 export const GameOfLife = () => {
   return (
-    <div className={Styles.example}>GameOfLife</div>
+    <div className={Styles.body}>
+      <div className={Styles.example}>GameOfLife</div>
+      <Grid />
+    </div>
   )
 }
--- pages/game-of-life/index.tsx
+++ pages/game-of-life/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import {GameOfLife as Component} from '../../components/GameOfLife'
 
+
 const GameOfLife: NextPage = () => {
   return (
     <Component></Component>
